33334a706d42a40de7df22a29a0293734fbe27d3
maddychoudhary <maddychoudhary0786@gmail.com>
2021-08-13 11:51:31 +0530

    build repo

---
+++ build/precache-manifest.5c6b1e2088a6588a7762a2a60e595734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9033fd5c1e117c8993bdb30e90baa1d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2950f8ba0ac1a3745b43",
+    "url": "/static/css/2.ee092dc8.chunk.css"
+  },
+  {
+    "revision": "98ad99a91daf37dfb6bb",
+    "url": "/static/css/main.60379d48.chunk.css"
+  },
+  {
+    "revision": "2950f8ba0ac1a3745b43",
+    "url": "/static/js/2.232210b8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.232210b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ad99a91daf37dfb6bb",
+    "url": "/static/js/main.3273bc64.chunk.js"
+  },
+  {
+    "revision": "f6ca8bb2e593fd4a8e53",
+    "url": "/static/js/runtime-main.dce5d66a.js"
+  }
+]);
